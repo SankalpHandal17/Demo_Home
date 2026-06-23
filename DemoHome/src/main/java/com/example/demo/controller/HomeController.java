f05d1a1d3ab72604b52dd1b694edad4a9f2796e2
handa <handa@LAPTOP-578KB0NF>
2026-06-23 09:58:59 +0530

    complete

---
+++ DemoHome/src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Home;
+import com.example.demo.service.HomeService;
+
+@RestController
+public class HomeController {
+
+	@Autowired
+	private HomeService ss;
+	
+	@GetMapping("/display")
+	public List<Home>displayHomes(){
+		return ss.display();
+	}
+	
+	@PostMapping("/add")
+	public void addHome(Home s) {
+		ss.add(s);
+	}
+}
